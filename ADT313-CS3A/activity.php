e6ade77ed218290984d49a1e2257da35eea889f9
Kim Ryan R. Nabo <147904349+Ryannabo@users.noreply.github.com>
2024-09-01 16:20:08 +0800

    resubmission of activity yesterday 01/08/2024

--- ADT313-CS3A/activity.php
+++ ADT313-CS3A/activity.php
@@ -1,122 +1,121 @@
 <h1>Activity</h1>
-<table align = "left" border = "1" cellpadding = "3" cellspacing = "2">   
+
+//revision of activity yesterday 01/08/2024
 
 <?php
-    $studentID = 1;
-    
-    $table = array(
-        "header"=>array(
-            "Student ID",
-            "Lastname",
-            "Middlename",
-            "Firstname",
-            "Course",
-            "Section"
-        ),
-        "body"=>array(
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-            array(
-                "lastname"=>"Lastname",
-                "middlename"=>"Middlename",
-                "firstname"=>"Firstnae",
-                "course"=>"Course",
-                "section"=>"Section"
-            ),
-             
-        )
 
-        );
-        echo "<tr>";
-        echo "<td>Student ID</td>";
-        echo "<td>" . $table["header"]["Lastname"] . "</td>";
-        echo "<td>" . $table["header"]["Firstname"] . "</td>";
-        echo "<td>" . $table["header"]['Firstname'] . "</td>";
-        echo "<td>" . $table["header"]['Firstname'] . "</td>";
-        echo "<td>" . $table["header"]['Firstname'] . "</td>";
-        echo "</tr>";
-       
-    foreach($table as $tables){.0
-        echo "<tr>";
-        echo "<td>$studentID</td>";
-        echo "<td>" . $table["body"]['lastname'] . "</td>";
-        echo "<td>$tables[body]['firstname']</td>";
-        echo "<td>$tables[body]['middlename']</td>";
-        echo "<td>$tables[body]['Course']</td>";
-        echo "<td>$tables[body]['Section']</td>";
-        echo "<tr>";
-        $studentID++;
-    }
+$table = array(
+    "header" => array(
+        "Student ID",
+        "Lastname",
+        "Middlename",
+        "Firstname",
+        "Course",
+        "Section"
+    ),
+    "body" => array(
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+        array(
+            "lastname" => "Lastname",
+            "middlename" => "Middlename",
+            "firstname" => "Firstnae",
+            "course" => "Course",
+            "section" => "Section"
+        ),
+
+    )
+
+);
+
+$studentID = 1;
 
 ?>
 
-
+<table border="1">
+    <thead>
+        <tr>
+            <?php foreach ($table['header'] as $header): ?>
+                <th><?php echo htmlspecialchars($header); ?></th>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($table['body'] as $row): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($studentID++); ?></td>
+                <?php foreach (array_keys($table['body'][0]) as $key): ?>
+                    <td><?php echo htmlspecialchars($row[$key]); ?></td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
